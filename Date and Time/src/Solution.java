b3c5fe07bb8d7935767b68735f8251ff4d28bf80
Kacper <kacper199902@gmail.com>
2019-03-05 22:31:01 +0000

    Day Determinant

---
+++ Date and Time/src/Solution.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        if (month == 1 || month == 2)
+            year = year - 1;
+        int yearFirst = year / 100;
+        int yearLast = year % 100;
+        int determinant = (int) (day + Math.floor(2.6 * (((month + 9) % 12) + 1) - 0.2) + yearLast
+                + Math.floor(yearLast / 4) + Math.floor(yearFirst / 4) - 2 * yearFirst) % 7;
+        determinant = Math.abs(determinant);
+        String dayInWords = "";
+        switch (determinant) {
+            case 0:
+                dayInWords = "SUNDAY";
+                break;
+            case 1:
+                dayInWords = "MONDAY";
+                break;
+            case 2:
+                dayInWords = "TUESDAY";
+                break;
+            case 3:
+                dayInWords = "WEDNESDAY";
+                break;
+            case 4:
+                dayInWords = "THURSDAY";
+                break;
+            case 5:
+                dayInWords = "FRIDAY";
+                break;
+            case 6:
+                dayInWords = "SATURDAY";
+        }
+        return (dayInWords);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
